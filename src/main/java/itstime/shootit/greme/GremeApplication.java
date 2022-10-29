3b34f106e31e94265864eea32bd23b361d846d13
ChaeyeonHan <kate2236e@naver.com>
2022-10-29 14:29:29 +0900

    Initial commit

---
+++ src/main/java/itstime/shootit/greme/GremeApplication.java
@@ -0,0 +1,13 @@
+package itstime.shootit.greme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GremeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GremeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/itstime/shootit/greme/GremeApplicationTests.java
@@ -0,0 +1,13 @@
+package itstime.shootit.greme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GremeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
