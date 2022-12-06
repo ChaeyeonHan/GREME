af72b0f844fd15ad7ff79c316ab5e760f2822ed5
Sangin <ksiisk99@gmail.com>
2022-12-06 13:59:57 +0900

    배포 테스트

--- src/main/java/itstime/shootit/greme/oauth/presentation/Oauth2Controller.java
+++ src/main/java/itstime/shootit/greme/oauth/presentation/Oauth2Controller.java
@@ -15,7 +15,7 @@ public class Oauth2Controller {
     public LoginResponse getUserInfo(HttpServletRequest request) {
         if (request.getHeader("accessToken") == null) {
             return new LoginResponse((String) request.getAttribute("email"));
-        }
+        }//test
         return new LoginResponse((String) request.getAttribute("email")); //이미 회원이라면 request에 유저에 대한 정보들이 있을 것이다. 그거를 담아서 넘기자
     }
 }
