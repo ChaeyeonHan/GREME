d04b828e56e7962919a61ebc93416f9cab4d3153
Sangin <ksiisk99@gmail.com>
2022-12-07 17:46:48 +0900

    feat: 클라이언트한테 네이버 AccessToken을 받아서 사용자 이메일 가져오기

--- src/main/java/itstime/shootit/greme/config/SecurityConfig.java
+++ src/main/java/itstime/shootit/greme/config/SecurityConfig.java
@@ -1,7 +1,5 @@
 package itstime.shootit.greme.config;
 
-import itstime.shootit.greme.oauth.handler.OAuth2SuccessHandler;
-import itstime.shootit.greme.oauth.application.CustomOauth2UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,9 +13,6 @@ import org.springframework.security.web.firewall.HttpFirewall;
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
-    private final CustomOauth2UserService customOauth2UserService;
-    private final OAuth2SuccessHandler oAuth2SuccessHandler;
-
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -31,15 +26,7 @@ public class SecurityConfig {
                 .csrf().disable()
                 .authorizeHttpRequests()
                 .antMatchers("/oauth2/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin().disable()
-                .oauth2Login()
-                .userInfoEndpoint()
-                .userService(customOauth2UserService)
-                .and()
-//                .defaultSuccessUrl("/oauth2-authroization-kakao") //로그인 실패 시
-                .successHandler(oAuth2SuccessHandler); //로그인 성공 시
+                .anyRequest().authenticated();
 
         return http.build();
     }
