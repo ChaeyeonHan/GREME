14152418565d1bcaf5f44e3c03f0974ec901fa88
Sangin <ksiisk99@gmail.com>
2022-12-20 14:24:43 +0900

    feat: 중복 닉네임 확인

---
+++ src/main/java/itstime/shootit/greme/user/application/UserService.java
@@ -0,0 +1,17 @@
+package itstime.shootit.greme.user.application;
+
+import itstime.shootit.greme.user.infrastructure.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    public void checkExistsUsername(String username) {
+        if (userRepository.existsByUsername(username)) {
+            throw new RuntimeException("중복");
+        }
+    }
+}
