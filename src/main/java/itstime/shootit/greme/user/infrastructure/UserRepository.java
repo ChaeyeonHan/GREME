e69275c5075ced00c1c6db79f8b80058560d4af0
Sangin <ksiisk99@gmail.com>
2022-12-20 22:06:45 +0900

    feat: oauth2 로그인 성공 시 기존 회원을 판별하여 로그인 응답 값 리턴

--- src/main/java/itstime/shootit/greme/user/infrastructure/UserRepository.java
+++ src/main/java/itstime/shootit/greme/user/infrastructure/UserRepository.java
@@ -10,4 +10,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
     boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
 }
