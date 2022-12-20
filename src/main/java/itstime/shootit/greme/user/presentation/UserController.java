ae38409948a102daee93277ee04eb731b6718364
Sangin <ksiisk99@gmail.com>
2022-12-20 14:25:38 +0900

    style: 스웨거 테스트 코드 삭제

--- src/main/java/itstime/shootit/greme/user/presentation/UserController.java
+++ src/main/java/itstime/shootit/greme/user/presentation/UserController.java
@@ -19,16 +19,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     private final UserService userService;
 
-    @Operation(summary = "스웨거 테스트",
-            parameters = {@Parameter(name = "temp", description = "임시 파라미터 입니다.")}
-    )
-    @GetMapping("")
-    public String swwagerTest(
-            @RequestParam("temp") String temp
-    ) {
-        return "swagger";
-    }
-
     @Operation(summary = "중복된 닉네임인지 조회",
             parameters = {@Parameter(name = "username", description = "유저 닉네임")}
     )
