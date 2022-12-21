b8b17553a0b290bc2f250cd5c5286bb380984fbd
ChaeyeonHan <kate2236e@naver.com>
2022-12-21 09:00:54 +0900

    feat: 참여가능 챌린지 api

---
+++ src/main/java/itstime/shootit/greme/challenge/presentation/ChallengeController.java
@@ -0,0 +1,27 @@
+package itstime.shootit.greme.challenge.presentation;
+
+import itstime.shootit.greme.challenge.application.ChallengeService;
+import itstime.shootit.greme.challenge.domain.Challenge;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/oauth2/challenge")
+public class ChallengeController {
+
+    private final ChallengeService challengeService;
+
+    @GetMapping("{userId}")
+    public List<Challenge> challenge(@PathVariable Long userId){
+        return challengeService.challenge(userId);
+    }
+
+
+}
