8be6f8f24e239d0f92d9daffad5ae1f85f9bbbc9
ChaeyeonHan <kate2236e@naver.com>
2022-12-21 09:52:37 +0900

    mod: 정렬 기준 추가

--- src/main/java/itstime/shootit/greme/challenge/ChallengUserRepository.java
+++ src/main/java/itstime/shootit/greme/challenge/ChallengUserRepository.java
@@ -10,6 +10,6 @@ import java.util.List;
 public interface ChallengUserRepository extends JpaRepository<ChallengeUser, Long> {
 
     @Query(value = "SELECT *  FROM Challenge c LEFT OUTER JOIN ChallengeUser cu on c.id = cu.challenge_id\n" +
-            "WHERE c.id NOT IN (SELECT cu.challenge_id FROM ChallengeUser cu WHERE cu.user_id=:userId);", nativeQuery = true)
+            "WHERE c.id NOT IN (SELECT cu.challenge_id FROM ChallengeUser cu WHERE cu.user_id=:userId) ORDER BY c.cur_num DESC;", nativeQuery = true)
     List<Challenge> mfindChallenge(Long userId);
 }
