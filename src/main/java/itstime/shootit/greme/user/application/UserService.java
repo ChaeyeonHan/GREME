b6a9e0b53f5ae017482847982962fe9d231289f2
Sangin <ksiisk99@gmail.com>
2022-12-22 17:05:55 +0900

    feat: 회원가입

--- src/main/java/itstime/shootit/greme/user/application/UserService.java
+++ src/main/java/itstime/shootit/greme/user/application/UserService.java
@@ -1,9 +1,13 @@
 package itstime.shootit.greme.user.application;
 
+import itstime.shootit.greme.user.domain.User;
+import itstime.shootit.greme.user.dto.request.SignUpReq;
 import itstime.shootit.greme.user.exception.ExistsUsernameException;
+import itstime.shootit.greme.user.exception.FailSignUpException;
 import itstime.shootit.greme.user.infrastructure.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,5 +18,18 @@ public class UserService {
         if (userRepository.existsByUsername(username)) {
             throw new ExistsUsernameException();
         }
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    public void signUp(SignUpReq signUpReq) {
+        try {
+            userRepository.save(User.builder()
+                    .email(signUpReq.getEmail())
+                    .username(signUpReq.getUsername())
+                    .build());
+        } catch (Exception e) {
+            throw new FailSignUpException();
+        }
+
     }
 }
--- src/main/java/itstime/shootit/greme/user/presentation/UserController.java
+++ src/main/java/itstime/shootit/greme/user/presentation/UserController.java
@@ -3,14 +3,16 @@ package itstime.shootit.greme.user.presentation;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import itstime.shootit.greme.common.error.exception.BusinessException;
 import itstime.shootit.greme.user.application.UserService;
+import itstime.shootit.greme.user.dto.request.SignUpReq;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -25,6 +27,19 @@ public class UserController {
     @GetMapping("/username/check")
     public ResponseEntity<Void> existsUsername(@RequestParam("username") String username) {
         userService.checkExistsUsername(username);
+        return ResponseEntity
+                .status(HttpStatus.NO_CONTENT)
+                .build();
+    }
+
+    @Operation(summary = "중복된 닉네임인지 조회",
+            responses = {
+                    @ApiResponse(responseCode = "422", description = "회원가입 실패", content = @Content(schema = @Schema(implementation = BusinessException.class))),
+            }
+    )
+    @PostMapping("/sign-up")
+    public ResponseEntity<Void> signUp(@RequestBody SignUpReq signUpReq) {
+        userService.signUp(signUpReq);
         return ResponseEntity
                 .status(HttpStatus.NO_CONTENT)
                 .build();
