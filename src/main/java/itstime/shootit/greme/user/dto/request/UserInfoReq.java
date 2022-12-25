e08df1085b7327200c41d346677e9ae7f8856d53
Sangin <ksiisk99@gmail.com>
2022-12-25 20:20:20 +0900

    refactor: api명세서 수정

--- src/main/java/itstime/shootit/greme/user/dto/request/UserInfoReq.java
+++ src/main/java/itstime/shootit/greme/user/dto/request/UserInfoReq.java
@@ -1,5 +1,6 @@
 package itstime.shootit.greme.user.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,7 +9,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserInfoReq {
+    @Schema(description = "성별 => 0(male), 1(female), 2(Anything)")
     private Integer genderType;
+
+    @Schema(description = "사는 지역")
     private String area;
+
+    @Schema(description = "앱 사용 목적")
     private String purpose;
 }
