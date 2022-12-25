1a872d8eb0f2488f4687fa059b8c8b419b82ad6a
ChaeyeonHan <kate2236e@naver.com>
2022-12-25 17:54:34 +0900

    feat: 챌린지 등록

--- src/main/java/itstime/shootit/greme/challenge/application/ChallengeService.java
+++ src/main/java/itstime/shootit/greme/challenge/application/ChallengeService.java
@@ -1,7 +1,11 @@
 package itstime.shootit.greme.challenge.application;
 
 import itstime.shootit.greme.challenge.ChallengUserRepository;
+import itstime.shootit.greme.challenge.ChallengeRepository;
+import itstime.shootit.greme.challenge.domain.ChallengeUser;
 import itstime.shootit.greme.challenge.dto.ChallengeSummary;
+import itstime.shootit.greme.challenge.exception.FailAddChallengeException;
+import itstime.shootit.greme.user.domain.User;
 import itstime.shootit.greme.user.exception.NotExistUserException;
 import itstime.shootit.greme.user.infrastructure.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +19,7 @@ import java.util.List;
 public class ChallengeService {
 
     private final ChallengUserRepository challengUserRepository;
+    private final ChallengeRepository challengeRepository;
     private final UserRepository userRepository;
 
     @Transactional(readOnly = true)
@@ -33,6 +38,21 @@ public class ChallengeService {
         }
 
         return challengUserRepository.mfindJoinChallenge(userId);
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    public void addChallenge(String email, Long challengeId){
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(NotExistUserException::new);
+
+        try {
+            challengUserRepository.save(ChallengeUser.builder()
+                    .challenge(challengeRepository.findById(challengeId).get())
+                    .user(user)
+                    .build());
+        } catch (Exception e){
+            throw new FailAddChallengeException();
+        }
     }
 
 
--- src/main/java/itstime/shootit/greme/challenge/presentation/ChallengeController.java
+++ src/main/java/itstime/shootit/greme/challenge/presentation/ChallengeController.java
@@ -1,12 +1,12 @@
 package itstime.shootit.greme.challenge.presentation;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import itstime.shootit.greme.challenge.application.ChallengeService;
 import itstime.shootit.greme.challenge.dto.ChallengeSummary;
+import itstime.shootit.greme.oauth.application.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,6 +17,7 @@ import java.util.List;
 public class ChallengeController {
 
     private final ChallengeService challengeService;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @GetMapping("/{userId}")
     public List<ChallengeSummary> challenge(@PathVariable Long userId){
@@ -26,6 +27,14 @@ public class ChallengeController {
     @GetMapping("/{userId}/join")
     public List<ChallengeSummary> joinChallenge(@PathVariable Long userId){
         return challengeService.joinChallenge(userId);
+    }
+
+    @Operation(summary = "챌린지 등록", parameters = {@Parameter(name = "accessToken", description = "액세스 토큰"),
+        @Parameter(name = "challengeId", description = "참여하려는 챌린지 id")}
+    )
+    @PostMapping("/join/{challengeId}")
+    public void addChallenge(@PathVariable Long challengeId, @RequestHeader("accessToken") String accessToken){
+        challengeService.addChallenge(jwtTokenProvider.getEmail(accessToken), challengeId);
     }
 
 
