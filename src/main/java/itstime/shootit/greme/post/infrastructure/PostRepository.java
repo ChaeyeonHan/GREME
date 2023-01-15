55411e32e9ef8db51ccdcee7423e1e27bf2aaad2
Sangin <ksiisk99@gmail.com>
2023-01-16 00:20:28 +0900

    fix: nativeQuery 테이블명 소문자로 변경

--- src/main/java/itstime/shootit/greme/post/infrastructure/PostRepository.java
+++ src/main/java/itstime/shootit/greme/post/infrastructure/PostRepository.java
@@ -22,18 +22,18 @@ public interface PostRepository extends JpaRepository<Post, Long> {
             "WHERE date_format(p.created_date, '%Y-%m-%d')=:date AND p.user_id=:userId", nativeQuery = true)
     Optional<PostRes> findPostByUserAndDate(@Param("userId") Long userId, @Param("date")String date);
 
-    @Query(value = "SELECT p.id, p.image FROM Post p WHERE p.user_id = :userId AND p.status = true ORDER BY p.created_date DESC LIMIT 5;", nativeQuery = true)
+    @Query(value = "SELECT p.id, p.image FROM post p WHERE p.user_id = :userId AND p.status = true ORDER BY p.created_date DESC LIMIT 5;", nativeQuery = true)
     List<GetPostRes> findRecentPostByUserEmail(Long userId);
 
-    @Query(value = "SELECT u.username, p.image, p.content, p.hashtag, p.created_date FROM Post p LEFT OUTER JOIN User u on u.id = p.user_id WHERE p.id = :post_id", nativeQuery = true)
+    @Query(value = "SELECT u.username, p.image, p.content, p.hashtag, p.created_date FROM post p LEFT OUTER JOIN User u on u.id = p.user_id WHERE p.id = :post_id", nativeQuery = true)
     GetPostSummaryRes findOnePost(Long post_id);
 
-    @Query(value = "SELECT p.image FROM Post p WHERE p.id=:post_id", nativeQuery = true)
+    @Query(value = "SELECT p.image FROM post p WHERE p.id=:post_id", nativeQuery = true)
     Optional<String> findImageById(@Param("post_id") Long post_id);
 
     void deleteByIdAndUser(Long id, User user);
 
-    @Query(value = "SELECT p.id, p.image, date_format(p.created_date, '%Y-%m') as createdDate FROM Post p WHERE p.user_id=:user_id ORDER BY p.id DESC", nativeQuery = true)
+    @Query(value = "SELECT p.id, p.image, date_format(p.created_date, '%Y-%m') as createdDate FROM post p WHERE p.user_id=:user_id ORDER BY p.id DESC", nativeQuery = true)
     List<PostInfoQuery> findAllByUserOrderByIdDesc(@Param("user_id") Long user_id);
 
     List<PostInfo> findByContentContainingOrHashtagContaining(String content, String hashtag);
