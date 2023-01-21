dbf3f68f6bfa57f2212cd498e8e113468f9b1df6
Sangin <ksiisk99@gmail.com>
2023-01-21 20:54:53 +0900

    fix: 홈피드 쿼리문 오류 수정

--- src/main/java/itstime/shootit/greme/challenge/infrastructure/ChallengeRepository.java
+++ src/main/java/itstime/shootit/greme/challenge/infrastructure/ChallengeRepository.java
@@ -5,6 +5,7 @@ import itstime.shootit.greme.challenge.dto.ChallengeTitle;
 import itstime.shootit.greme.challenge.dto.response.GetChallengeSummaryRes;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,11 +22,11 @@ public interface ChallengeRepository extends JpaRepository<Challenge, Long> {
     void numDeleted(List<Long> challengeIds);
 
     @Query(value = "SELECT c.id FROM challenge c INNER JOIN challenge_user cu ON c.id=cu.challenge_id WHERE cu.user_id=:userId ORDER BY c.id DESC", nativeQuery = true)
-    List<Long> findParticipatingChallenge(Long userId);
+    List<Long> findParticipatingChallenge(@Param("userId") Long userId);
 
-    @Query(value = "SELECT c.id. c.title FROM challenge c INNER JOIN challenge_post cp ON c.id=cp.challenge_id GROUP BY cp.challenge_id ORDER BY COUNT(cp.id) DESC LIMIT 1", nativeQuery = true)
+    @Query(value = "SELECT c.id, c.title FROM challenge c INNER JOIN challenge_post cp ON c.id=cp.challenge_id GROUP BY cp.challenge_id ORDER BY COUNT(cp.id) DESC LIMIT 1", nativeQuery = true)
     ChallengeTitle findPopularityChallenge();
 
     @Query(value = "SELECT c.id, c.title FROM challenge c INNER JOIN challenge_post cp ON c.id=cp.challenge_id WHERE cp.challenge_id IN :myChallenges GROUP BY cp.challenge_id ORDER BY COUNT(cp.id) DESC LIMIT 1", nativeQuery = true)
-    ChallengeTitle findParticipatingPopularityChallenge(List<Long> myChallenges);
+    ChallengeTitle findParticipatingPopularityChallenge(@Param("myChallenges") List<Long> myChallenges);
 }
